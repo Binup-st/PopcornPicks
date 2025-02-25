02aa18a578d4cb8a803d5e2d872df8db420dc1ca
Binup-st <2077csit07@hcoe.edu.np>
2025-02-26 02:24:38 +0545

    pre installation

--- api/index.js
+++ api/index.js
@@ -1,40 +1,49 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
-import userRoutes from './routes/user.routes.js'
-import authRoutes from './routes/auth.routes.js'
-import movieRoutes from './routes/watchlist.routes.js'
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import movieRoutes from "./routes/watchlist.routes.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
-dotenv.config()
+dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => console.log("MongoDB is connected"))
   .catch((err) => console.log(err.message));
 
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
 
-app.use('/api/user', userRoutes)
+app.use("/api/user", userRoutes);
 
-app.use('/api/auth', authRoutes)
+app.use("/api/auth", authRoutes);
 
-app.use('/api/movie', movieRoutes)
+app.use("/api/movie", movieRoutes);
 
-app.use((err, req, res, next)=>{
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client", "dist", "index.html"));
+});
+
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal server error";
 
   res.status(statusCode).json({
     success: false,
     statusCode,
-    message
-  })
-})
+    message,
+  });
+});
